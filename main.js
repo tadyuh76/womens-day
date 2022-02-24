7cd0a9c6b23f5503931dd251be11866c3fd12e25
tadyuh76 <tadyuh76@gmail.com>
2022-02-24 21:56:11 +0700

    update code

--- main.js
+++ main.js
@@ -1,13 +1,97 @@
+const music = new Audio('./assets/ngaydautien.mp3')
+music.play()
+
 const boys = [
   {
-    name: 'Đạt Huy',
-    img: 'dathuy.jpeg'
-  },
+    name: "Ngọc Bảo",
+    image: "ngocbao.jpeg"
+},
   {
-    name: 'Phước Tắc',
-    img: 'phuoctai.jpeg'
+    name: "Tiến Cư",
+    image: "tiencu.jpeg"
+},
+  {
+    name: "Việt Cường",
+    image: "vietcuong.jpeg"
+},
+  {
+    name: "Quốc Đại",
+    image: "quocdai.jpeg"
+},
+  {
+    name: "Công Danh",
+    image: "congdanh.jpeg"
+},
+  {
+    name: "Nhật Đông",
+    image: "nhatdong.jpeg"
+},
+  {
+    name: "Văn Đức",
+    image: "vanduc.jpeg"
+},
+  {
+    name: "Xuân Duy",
+    image: "xuanduy.jpeg"
+},
+  {
+    name: "Đạt Huy",
+    image: "dathuy.jpeg"
+},
+  {
+    name: "Huy Hoàng",
+    image: "huyhoang.jpeg"
+},
+  {
+    name: "Minh Khang",
+    image: "minhkhang.jpeg"
+},
+  {
+    name: "Trác Khang",
+    image: "trackhang.jpeg"
+},
+  {
+    name: "Anh Kiệt",
+    image: "anhkiet.jpeg"
+},
+  {
+    name: "Quang Minh",
+    image: "quangminh.jpeg"
+},
+  {
+    name: "Bá Lân",
+    image: "balan.jpeg"
+},
+  {
+    name: "Mậu Cường",
+    image: "maucuong.jpeg"
+},
+  {
+    name: "Trọng Nhân",
+    image: "trongnhan.jpeg"
+},
+  {
+    name: "Phước Tài",
+    image: "phuoctai.jpeg"
+},
+  {
+    name: "Minh Trường",
+    image: "minhtruong.jpeg"
+}, {
+    name: "Minh Trường",
+    image: "minhtruong.jpeg"
+},
+  {
+    name: "Thầy Thịnh fake",
+    image: "huuthinh.jpeg"
+},
+  {
+    name: "Thầy Thịnh real",
+    image: "thaythinh.jpeg"
+  }, {
+    name: "Thầy Thịnh real",
+    image: "thaythinh.jpeg"
   },
-
 ]
 boys.sort(() => 0.5 - Math.random())
 
@@ -16,9 +100,13 @@ const boyImage = document.querySelector('.boy-image')
 let index = 0;
 
 const popTheBoy = () => {
-  console.log("work!")
-  boyNameSpan.innerHTML = boys[index].name;
-  boyImage.src = `./assets/${boys[index].img}`
+  if (index >= 23) {
+    boyNameSpan.innerHTML = "Hết rồiii. Nhớ like, share và subscribe cho tụi con trai mình nha :D"
+    boyImage.src = `./assets/end.jpg`
+    return
+  }
 
-  index < boys.length - 1 ? index++ : null;
+  boyNameSpan.innerHTML = boys[index].name;
+  boyImage.src = `./assets/${boys[index].image}`
+  index++
 }
